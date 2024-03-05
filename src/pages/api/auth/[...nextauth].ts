695f700e086d372974e2aea67267867b58b2c97e
abubuh <cristian_caht@hotmail.com>
2024-03-05 02:00:39 -0600

    Add file with NextAuth provider with discord

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,17 @@
-import NextAuth from "next-auth";
-
-import { authOptions } from "~/server/auth";
-
-export default NextAuth(authOptions);
+import NextAuth from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    // })
+    // ...add more providers here
+  ],
+}
+export default NextAuth(authOptions)
