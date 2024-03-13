476e968625d4b923720baf0a7d65ddef274806d9
abubuh <cristian_caht@hotmail.com>
2024-03-12 20:24:15 -0500

    Type changes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID! as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET! as string,
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
     }),
     // GoogleProvider({
     //   clientId: process.env.GOOGLE_CLIENT_ID as string,
