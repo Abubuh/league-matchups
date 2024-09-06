2a161a33ba85f5e57ecfac8582245fc347a0c8f5
abubuh <cristian_caht@hotmail.com>
2024-09-06 04:35:14 -0500

    Add second option to link routing

--- src/components/ChampImage.tsx
+++ src/components/ChampImage.tsx
@@ -3,21 +3,31 @@ import Link from "next/link";
 import React from "react";
 import { CHAMP_IMAGE_URL } from "~/config";
 
-export const ChampionsImage = ({ champions }: { champions: Champion[] }) => {
+export const ChampionsImage = ({
+  champions,
+  dataToDisplay,
+}: {
+  champions: Champion[];
+  dataToDisplay: string;
+}) => {
   return (
-    <div className={`mx-auto grid grid-cols-5`}>
+    <div className="mx-auto grid grid-cols-5">
       {champions?.map(({ name, key, id }) => {
         return (
           <Link
-            href={`/posts/${id}`}
+            href={`${
+              dataToDisplay === "Posts" ? `/posts/${id}` : `/champInfo/${name}`
+            }`}
             key={key}
             className="flex flex-col py-5 transition hover:scale-125"
           >
-            <p className="text-center text-black dark:text-white">{name}</p>
+            <p className="text-center text-black transition delay-200 ease-in-out dark:text-white">
+              {name}
+            </p>
             <img
               src={CHAMP_IMAGE_URL + key + ".png"}
-              className="mx-auto w-1/2"
-              alt=""
+              className="mx-auto w-1/2 rounded-sm"
+              alt={`${name}`}
               width={1280}
               height={1080}
             />
