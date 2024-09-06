c3920577c5780c1c4a9c6b203f3961190afc5168
abubuh <cristian_caht@hotmail.com>
2024-09-06 05:33:35 -0500

    Add query to fetch champ data

--- src/server/api/routers/champion.ts
+++ src/server/api/routers/champion.ts
@@ -29,12 +29,33 @@ export const championRouter = createTRPCRouter({
       return { data: name };
     }), 
 
+  // getByName: publicProcedure
+  // .input(z.object({ key: z.string().min(1) }))
+  // .query(async ({ ctx, input }) => {
+  //   const champion = await ctx.db.champion.findUnique({
+  //     where: { name: input.key },
+  //   });
+  //   return { champion };
+  // }),
+
   getByName: publicProcedure
-  .input(z.object({ key: z.string().min(1) }))
-  .query(async ({ ctx, input }) => {
-    const champion = await ctx.db.champion.findUnique({
-      where: { name: input.key },
-    });
-    return { champion };
+  .input(z.object({name: z.string().min(1)}))
+  .query(async ({ ctx, input}) => {
+    const championData = await ctx.db.champion.findUnique({
+      where: {name: input.name}
+    })
+    const abilities = await ctx.db.skill.findMany({
+      where: {championId: championData?.id}
+    })
+    const championFullData = {
+      name: championData?.name,
+      key: championData?.key,
+      id: championData?.id,
+      championAbilities: abilities
+    }
+    return {
+      data: championFullData
+    }
   }),
+
 });
