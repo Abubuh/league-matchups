9bd71050005c812b269c67ba175854f320eb3490
abubuh <cristian_caht@hotmail.com>
2024-09-06 05:31:33 -0500

    Change style

--- src/components/RoutingButton.tsx
+++ src/components/RoutingButton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const RoutingButton = ({ text, url }: { text: string; url: string }) => {
   return (
     <Link href={url}>
-      <button className="text-md max-w-[150px] rounded-md border-[2.3px] border-black bg-gray-300 px-5 py-1 text-black">
+      <button className="text-md max-w-[150px] rounded-md border-[2.3px] border-black bg-gray-200 px-5 py-1 text-black transition delay-300  ease-in-out hover:opacity-75 dark:bg-slate-400">
         {text}
       </button>
     </Link>
